ea04a238a4dbfeed8872fe6d29037ec6e1db7a63
h0diush <h0diush@yandex.ru>
2020-12-30 00:18:33 +0300

    final

--- homework.py
+++ homework.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+logging.basicConfig(level=logging.INFO)
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -18,6 +18,7 @@ def parse_homework_status(homework):
     homework_name = homework.get('lesson_name')
     if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
+        
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
@@ -42,13 +43,14 @@ def send_message(message, bot_client):
 def main():
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     # проинициализировать бота здесь
-    current_timestamp = int(time.time()) - 2592000 # начальное значение timestamp
+    current_timestamp = int(time.time()) # начальное значение timestamp
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]), bot_client)
+                logging.info('Сообщение отправлено')
             current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
             time.sleep(300)  # опрашивать раз в пять минут
 
